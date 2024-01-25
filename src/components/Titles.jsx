5a9d1f0315d590d98f95c069508f9e9c647e8f71
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-01-25 15:17:34 +0000

    add Title component for changing title dynamicly

---
+++ src/components/Titles.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Titles = ({title}) => {
+  return (
+    <div className='flex flex-col items-center justify-center mt-10 '>
+      <h2 className='text-black/80 font-bold text-4xl tracking-widest mb-3 '>{title}</h2>
+      <span className="flex items-center justify-center h-1 w-16 md:w-20  bg-gray-500"></span>
+    </div>
+  );
+}
+
+export default Titles
