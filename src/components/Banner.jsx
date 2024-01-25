97526527fc5d953557b10f21c9d8933ceb8b4ca6
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-01-25 13:02:29 +0000

    add Banner component for using in hero component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Banner = ({children,title,subtitle}) => {
+  return (
+    <div className="flex gap-4 flex-col items-center justify-center backdrop-opacity-10 backdrop-invert bg-black/30 md:w-[40%] w-full h-auto p-10 mx-4 md:mx-0">
+      <h1 className="text-white font-bold text-lg md:text-5xl md:tracking-wide">
+       {title}
+      </h1>
+      <span className="flex items-center justify-center h-1 w-16 md:w-40  bg-white"></span>
+      <p className="text-sm md:text-lg font-medium">
+       {subtitle}
+      </p>
+      {children}
+      
+    </div>
+  );
+}
+
+export default Banner
