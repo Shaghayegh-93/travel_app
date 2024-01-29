c4732e1bcc61b3fb1bfa56f15b452d7dfb60ec35
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-01-29 22:35:10 +0000

    improve logic

--- src/components/FeaturedRooms.jsx
+++ src/components/FeaturedRooms.jsx
@@ -1,21 +1,20 @@
-import React, { useState } from "react";
-import items from "../../data";
+import React from "react";
 import SingleRoom from "./singleRoom";
 import Titles from "./Titles";
+import { useRoomList } from "../context/RoomListProvider";
+import Loading from "./Loading";
 const FeaturedRooms = () => {
-//   const [featuredRoomList, setFeaturedRoomList] = useState([]);
-  const updated = items.filter((item) => item.fields.featured);
-  console.log(updated)
-  console.log(items);
+  const { roomList, featuredRoomList, isLoading } = useRoomList();
+
+const rooms= featuredRoomList?.map((item) => {
+            return <SingleRoom item={item} key={item.id} />;
+          })
   return (
     <div className="flex flex-col">
       <Titles title="Featured Rooms" />
       <div className="grid grid-cols-1 md:grid-cols-3 max-w-[1400px] py-16 px-4 m-auto gap-10 gap-y-20">
-        {updated?.map((item) => {
-          return <SingleRoom item={item} key={item.sys.id} />;
-        })}
+        {isLoading ? <Loading /> : rooms}
       </div>
-      
     </div>
   );
 };
