2bc5ade8154b8e2c696af7332b269b8efd8beeed
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-01-30 20:37:47 +0000

    add a class for grid

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        "auto-fill-270": "repeat(auto-fill, minmax(270px, 1fr))",
+      },
+      plugins: [],
+    },
   },
-  plugins: [],
 };
