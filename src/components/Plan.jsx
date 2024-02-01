6f2369a2e001978df38c46f243d539e9bed6ea7c
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-02-01 20:08:06 +0000

    import and add Titles component

--- src/components/Plan.jsx
+++ src/components/Plan.jsx
@@ -4,10 +4,13 @@ import plan2 from "../assets/images/plan2.jpg";
 import plan3 from "../assets/images/plan3.jpg";
 import plan4 from "../assets/images/plan4.jpg";
 import plan5 from "../assets/images/plan5.jpg";
+import Titles from "./Titles";
 
 const Plan = () => {
   return (
-    <div className="max-w-[1400px] py-16 px-4 m-auto grid lg:grid-cols-2 gap-4">
+    <div className="flex-flex-col ">
+      <Titles title="The best places to visit"/>
+<div className="max-w-[1400px] py-16 px-4 m-auto grid lg:grid-cols-2 gap-4">
       <div className="grid grid-cols-2 grid-rows-6 h-[80vh]">
         <img
           className="object-cover w-full h-full row-span-3 p-2"
@@ -36,7 +39,7 @@ const Plan = () => {
         />
       </div>
       <div className="flex h-full flex-col justify-center">
-        <h3 className="text-5xl md:text-6xl font-bold">Plan Your Next Trip</h3>
+        <h3 className="text-xl md:text-2xl font-bold">Plan Your Next Trip</h3>
         <p className="text-2xl py-6">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
           quae?
@@ -47,16 +50,11 @@ const Plan = () => {
           iste numquam aliquam quod atque porro laudantium consequatur provident
           mollitia inventore.
         </p>
-        {/* <div>
-          <button className="border-black mr-4 hover:shadow-xl">
-            Learn More
-          </button>
-          <button className="border-black bg-black text-white hover:shadow-xl">
-            Book Your Trip
-          </button>
-        </div> */}
+       
       </div>
     </div>
+    </div>
+    
   );
 };
 
