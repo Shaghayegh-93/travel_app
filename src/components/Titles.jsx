7f20b47e95f9c2906a00d6d2d4110a2a3c0e3f90
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-02-01 20:06:23 +0000

    improve ui

--- src/components/Titles.jsx
+++ src/components/Titles.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Titles = ({title}) => {
+const Titles = ({ title }) => {
   return (
-    <div className='flex flex-col items-center justify-center mt-10 '>
-      <h2 className='text-black/80 font-bold text-4xl tracking-widest mb-3 '>{title}</h2>
-      <span className="flex items-center justify-center h-1 w-16 md:w-20  bg-gray-500"></span>
+    <div className="flex flex-col items-center justify-center mt-10 ">
+      <h2 className="text-black/80 font-bold text-4xl tracking-widest mb-3 font-Gilda text-center">
+        {title}
+      </h2>
+      <span className="flex items-center justify-center h-1 w-16 md:w-28  bg-gray-500"></span>
     </div>
   );
-}
+};
 
-export default Titles
+export default Titles;
