f303ad21c1657902dde667f912c2f823c2c748a7
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-02-01 18:37:51 +0000

    add font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,14 @@ module.exports = {
       gridTemplateColumns: {
         "auto-fill-270": "repeat(auto-fill, minmax(270px, 1fr))",
       },
+      fontFamily: {
+        Inter: ["Inter"],
+        system: ["system-ui", "sans-serif"],
+        Avenir: ["Avenir", "sans-serif"],
+        Helvetica: ["Helvetica", "sans-serif"],
+        Arial: ["Arial", "sans-serif"],
+        Gilda: ["Gilda-Display"],
+      },
       plugins: [],
     },
   },
