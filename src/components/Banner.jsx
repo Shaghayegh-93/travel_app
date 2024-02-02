d69e9b195c8c38d42036f88c94296cf504ca01ad
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-02-02 18:02:45 +0000

    clean code

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const Banner = ({children,title,subtitle}) => {
+const Banner = ({ children, title, subtitle }) => {
+ 
   return (
     <div className="flex gap-4 flex-col items-center justify-center backdrop-opacity-10 backdrop-invert bg-black/30 md:w-[40%] w-full h-auto p-10 mx-4 md:mx-0">
-      <h1 className="font-Gilda text-white font-bold text-lg md:text-5xl md:tracking-wide">
-       {title}
+      <h1 className="font-Gilda text-white font-bold text-lg md:text-4xl md:tracking-tight">
+        {title?.toLocaleUpperCase()}
       </h1>
       <span className="flex items-center justify-center h-1 w-16 md:w-40  bg-white"></span>
-      <p className="text-sm md:text-lg font-medium">
-       {subtitle}
-      </p>
+      <p className="text-sm md:text-lg font-medium">{subtitle}</p>
       {children}
-      
     </div>
   );
-}
+};
 
-export default Banner
+export default Banner;
