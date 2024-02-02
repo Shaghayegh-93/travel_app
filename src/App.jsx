1b5b25bab455e6ee180ffb0d3d89ca51ef429dcf
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-02-02 18:03:20 +0000

    clean code

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import "./App.css";
-
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-// import RoomsInfo from "./components/RoomsInfo";
-// import Rooms from "./components/Rooms";
 import RoomListPage from "./pages/RoomListPage";
 import SingleRoomPage from "./pages/SingleRoomPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import RoomListProvider from "./context/RoomListProvider";
 import ReservationPage from "./pages/ReservationPage";
 import UserListProvider from "./context/UserListProvider";
-import  { Toaster } from "react-hot-toast";
-import Rooms from "./components/Rooms";
+import { Toaster } from "react-hot-toast";
 import GallerPage from "./pages/GallerPage";
-
 
 function App() {
   return (
@@ -27,10 +22,10 @@ function App() {
             <Route path="rooms" element={<RoomListPage />} />
             <Route path="rooms/:slug" element={<SingleRoomPage />} />
             <Route
-              path="rooms/:slug/reservation"
+              path={"rooms/:slug/reservation"}
               element={<ReservationPage />}
             />
-            <Route path="gallery" element={<GallerPage/>}/>
+            <Route path="gallery" element={<GallerPage />} />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </div>
