e06e2363d78ad900a3c193b0b1b7340d83dbd374
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-02-02 18:01:05 +0000

    add style

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import myGif from "../assets/images/loading-arrow.gif";
 
-
 const Loading = () => {
-//   const { isLoading } = useRoomList();
   return (
     <div>
-      <div>
+      <div className="flex flex-col items-center justify-center">
         <img src={myGif} alt="loading..." />
-
+        <p>Your Data Is Loading</p>
       </div>
     </div>
   );
