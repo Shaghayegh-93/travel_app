5680bb6ccbb772ee1e1d4da8b06beaf9e5143e4d
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-02-02 18:01:30 +0000

    clean code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,8 @@
 import React, { useState } from "react";
-import hero from "../assets/images/hero.jpeg";
-import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { addDays, compareAsc, format } from "date-fns";
-import { Link, NavLink, useLocation } from "react-router-dom";
 
 const Hero = ({ backgroundImage, children }) => {
-  // const location = useLocation();
-  // const defaultBackground = location.pathname === "/";
-  // const roomBackground = location.pathname === "/rooms";
-  // const singleRoomBackground = location.pathname === "/rooms/:slug";
+
 
   return (
     <header
