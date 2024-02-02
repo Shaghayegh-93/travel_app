21294dccbdd633befe5e557a0824d7d40d9e3c94
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-02-02 17:54:48 +0000

    improve logic and clean code

--- src/pages/SingleRoomPage.jsx
+++ src/pages/SingleRoomPage.jsx
@@ -2,37 +2,41 @@ import React, { useEffect } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import Layout from "../components/Layout";
 import Hero from "../components/Hero";
-import items from "../../data";
 import { useRoomList } from "../context/RoomListProvider";
-import Loading from "../components/Loading";
 import Banner from "../components/Banner";
-import defaultBackgroundImage from "../assets/images/room-12.jpeg";
 
 const SingleRoomPage = () => {
   const { slug } = useParams();
-  const navigate = useNavigate();
 
-  const { isLoading, roomList, getSingleRoom, room } = useRoomList();
+  const {
+   
+    getSingleRoom,
+    room,
+    getBookedRoom,
+  } = useRoomList();
+  //   useRoomList();
   useEffect(() => {
     getSingleRoom(slug);
   }, [slug]);
+ 
+
+ 
 
   if (!room) return <p>choose a room</p>;
   return (
     <div>
       <Layout>
-        <Hero
-          backgroundImage={room[0]?.images && room[0].images[0]}
-
-          
-        >
+        <Hero backgroundImage={room[0]?.images && room[0].images[0]}>
           <Banner title={room[0]?.name}>
             <div className="flex w-full items-center justify-center gap-2 md:gap-4">
-              <button className="py-2 font-medium border-2 hover:bg-black hover:text-white transition ease-in duration-300 w-1/2 ">
+              <button className="py-2 font-medium border-2 bg-black hover:bg-white text-white hover:text-black transition ease-in duration-300 w-1/2 ">
                 <Link to="/rooms">BACK TO ROOMS</Link>
               </button>
-              <button className="py-2 font-medium border-2 hover:bg-black hover:text-white transition ease-in duration-300 w-1/2">
-                <Link to={`/rooms/${slug}/reservation`}>BOOk ROOM</Link>
+              <button
+                onClick={getBookedRoom}
+                className="py-2 font-medium border-2 bg-black text-white hover:bg-white hover:text-black transition ease-in duration-300 w-1/2"
+              >
+                <Link to={`/rooms/${slug}/reservation`}> BOOk ROOM</Link>
               </button>
             </div>
           </Banner>
@@ -45,28 +49,28 @@ const SingleRoomPage = () => {
           </div>
           <div className="flex flex-col md:flex-row gap-10">
             <div className=" md:w-2/4 flex flex-col">
-              <h3>Details</h3>
+              <h3 className="font-bold text">Description</h3>
               <p>{room[0]?.description}</p>
             </div>
             <div className="md:w-1/4 flex flex-col">
-              <h3>Details</h3>
+              <h3 className="font-bold text">Details</h3>
               <ul className="flex flex-col">
                 <li>
-                  <span>Price:</span>${room[0]?.price}
+                  <span className="font-bold ">Price:</span>${room[0]?.price}
                 </li>
                 <li>
-                  <span>Size:</span>
+                  <span className="font-bold ">Size:</span>
                   {room[0]?.size}
                 </li>
                 <li>
-                  <span>Max Capacity:</span>
+                  <span className="font-bold ">Max Capacity:</span>
                   {room[0]?.capacity > 1
                     ? `${room[0]?.capacity} People`
                     : `${room[0]?.capacity} Person`}
                 </li>
                 <li>
                   {room[0]?.pets ? (
-                    <span>Pets Allowed</span>
+                    <span className="font-bold ">Pets Allowed</span>
                   ) : (
                     <span>Pets Not Allowed</span>
                   )}
@@ -78,7 +82,7 @@ const SingleRoomPage = () => {
             </div>
           </div>
           <div className="flex flex-col w-full">
-            <h3>Extras</h3>
+            <h3 className="font-bold text">Extras</h3>
             <ul className="grid grid-cols-1 md:grid-cols-3 gap-y-2">
               {room[0]?.extras.map((extra) => (
                 <li>-{extra}</li>
