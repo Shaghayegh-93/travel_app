ebcccb3e5ea658560e7f367dfd0517f8f61dd961
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-02-02 18:01:55 +0000

    clean code

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useRoomList } from "../context/RoomListProvider";
 import Titles from "./Titles";
-import { Link } from "react-router-dom";
 
 const Filters = () => {
   const {
