70e3e1d498bd20c750b2262e04982c68eb6a3db5
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-02-05 23:18:55 +0000

    improve style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,9 @@ import React, { useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 
 const Hero = ({ backgroundImage, children }) => {
-
-
   return (
     <header
-      className="bg-no-repeat min-h-[calc(100vh-80px)] bg-center bg-cover flex items-center justify-center "
+      className="bg-no-repeat min-h-[calc(100vh-200px)] bg-center bg-cover flex items-center justify-center "
       style={{
         backgroundImage: `url(${backgroundImage})`,
       }}
