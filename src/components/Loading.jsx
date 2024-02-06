d1f16143d61f6574de5ea463e2f0e65cd1cf9049
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-02-06 10:38:28 +0000

    improve style

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,7 +4,7 @@ import myGif from "../assets/images/loading-arrow.gif";
 const Loading = () => {
   return (
     <div>
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center mx-auto">
         <img src={myGif} alt="loading..." />
         <p>Your Data Is Loading</p>
       </div>
